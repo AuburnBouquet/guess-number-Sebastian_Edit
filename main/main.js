0187308131234b026c4898ded3762111a1da6ae6
Sebastian Kim <sebastian_jr@yeah.net>
2018-04-14 14:49:28 +0800

    guess_number completed

--- main/main.js
+++ main/main.js
@@ -1,6 +1,41 @@
 // Write your cade below:
-function main() {};
+function main(users_number, original_number) {
+    let arr1 = users_number.split("");
+    let arr2 = original_number.split("");
+    let result = {A: 0, B: 0};
+    for(var i in arr1){
+        let temp1 = i; let temp2 = arr1[i];
+        for(var j in arr2){
+            if(temp1 === j && temp2 === arr2[j]){
+                result.A++;
+            }
+            if(temp1 !== j && temp2 === arr2[j]){
+                result.B++;
+            }
+        }
+    }
+    let feedback = result.A+'A'+result.B+'B';
+    return feedback;
+};
 
-module.exports = {
-    main
+function getArray(){  // 随机生成4位不重复的数字
+    let arr = []; getRandom(arr);
+    let str = '';
+    (arr).forEach((e)=>{ str += e.toString(); });
+    return str;
 }
+function getRandom(array){
+    let random = Math.floor(Math.random()*10); // 产生随机数并向下取整
+
+    while(array.length < 4){                         //判断数组长度
+        for(var i=0;i<=array.length;i++){             //遍历数组
+            if(random==array[i]) break;                   //比较随机数
+            else {
+                if(i==array.length)
+                {array.push(random);break;}
+            }
+        };
+        getRandom(array);
+    }
+}
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,31 @@
-const main = require('../main/main');
+const main = require('../main/main.js');
 
 describe('main()', () => {
+    it('the number is 1234 and the answer is 1234', function () {
+        let input = '1234';
+        let result = main('1234', input);
+        expect(result).toEqual('4A0B');
+    });
 
+    it('the number is 1234 and the answer is 4321', function () {
+        let input = '1234';
+        let result = main('4321', input);
+        expect(result).toEqual('0A4B');
+    });
+
+    it('the number is 1234 and the answer is 5678', function () {
+        let input = '1234';
+        let result = main('5678', input);
+        expect(result).toEqual('0A0B');
+    });
+    it('the number is 1234 and the answer is 1536', function () {
+        let input = '1234';
+        let result = main('1536', input);
+        expect(result).toEqual('2A0B');
+    });
+    it('the number is 0789 and the answer is 7089', function () {
+        let input = '0789';
+        let result = main('7089', input);
+        expect(result).toEqual('2A2B');
+    });
 });
